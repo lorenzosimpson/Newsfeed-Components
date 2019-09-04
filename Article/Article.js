f7cd4e7b7d12a743999e25803b0b2e9a2ebe687f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-04 15:31:25 -0400

    Creates function outline, adds parameters and creates elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,22 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function createArticle(title, date, p1, p2, p3) {
+
+  //create elements 
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const articleP1 = document.createElement('p');
+  const articleP2 = document.createElement('p');
+  const articleP3 = document.createElement('p');
+  const expandButton = document.createElement('span');
+
+
+
+
+
+
+
+}
